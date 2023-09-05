887487c98cb94a354ee2c80571d2da6a9bf38ac9
Cody <66153585+nonstickcode@users.noreply.github.com>
2023-09-05 14:18:41 -0700

    form changed some but still too tall on ipad

--- Date Tracker/New Data Entry Form/NewDataEntryForm.swift
+++ Date Tracker/New Data Entry Form/NewDataEntryForm.swift
@@ -43,144 +43,150 @@ struct NewDataEntryForm: View {
     
     
     var body: some View {
-        NavigationView {
-            VStack {
+        
+        
+        VStack {
+            
+            //
+            Text("Add New Event")
+                .formHeaderStyle()
+                .padding(.bottom, 4)
+                .padding(.top, 8)
+            
+            Text("Swipe down to dismiss")
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .padding(.top, 4)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            // Detect swipe down gesture
+                            if value.translation.height > 100 {
+                                presentationMode.wrappedValue.dismiss()
+                            }
+                        }
+                )
+            Image(systemName: "chevron.compact.down")
+                .font(.system(size: 30))
+            // add .scaleEffect(.bounce) when ios 17 drops
+                .padding(2)
+            
+            Form {
+                Section(header: Text("Name of Person or Event").formRegularStyle()) {
+                    TextField("enter name here", text: $newEventName)
+                        .font(.custom("RobotoMono-Italic", size: 14))
+                }
                 
-                //
-                Text("Add New Event")
-                    .formHeaderStyle()
-                    .padding(.bottom, 4)
-                    .padding(.top, 8)
+                Section(header: Text("Preferred pronoun for person or event").formRegularStyle()) {
+                    TextField("enter pronoun here", text: $newPreferredPronoun)
+                        .font(.custom("RobotoMono-Italic", size: 14))
+                }
                 
-                Text("Swipe down to dismiss")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                    .padding(.top, 4)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                // Detect swipe down gesture
-                                if value.translation.height > 100 {
-                                    presentationMode.wrappedValue.dismiss()
-                                }
-                            }
-                    )
-                Image(systemName: "chevron.compact.down")
-                    .font(.system(size: 30))
-                // add .scaleEffect(.bounce) when ios 17 drops
-                    .padding(2)
-                
-                Form {
-                    Section(header: Text("Name of Person or Event").formRegularStyle()) {
-                        TextField("enter name here", text: $newEventName)
-                            .font(.custom("RobotoMono-Italic", size: 14))
-                    }
-                    
-                    Section(header: Text("Preferred pronoun for person or event").formRegularStyle()) {
-                        TextField("enter pronoun here", text: $newPreferredPronoun)
-                            .font(.custom("RobotoMono-Italic", size: 14))
-                    }
-                    
-                    Section(header: Text("Event Date, upcoming or past").formRegularStyle()) {
-                        VStack {
-                            Picker("Month", selection: $selectedMonth) {
-                                ForEach(months, id: \.self) { month in
-                                    Text(month).tag(month)
-                                       
-                                }
-                            }
-                            
-                            
-                            Picker("Day", selection: $selectedDay) {
-                                ForEach(1..<32) { day in
-                                    Text("\(day)").tag(day)
-                                        
-                                }
-                            }
-                            
-                            
-                            Picker("Year", selection: $selectedYear) {
-                                ForEach(1920..<2100) { year in
-                                    Text(String(format: "%ld", locale: Locale(identifier: "en_US_POSIX"), year)).tag(year)
-                                        
-                                    
-                                }
-                            }
-                            
-                            
-                        }
-                        .onAppear {
-                            // Initialize the custom picker with the existing date
-                            let components = Calendar.current.dateComponents([.day, .month, .year], from: newEventDate)
-                            selectedMonth = months[(components.month ?? 1) - 1]
-                            selectedDay = components.day ?? 1
-                            selectedYear = components.year ?? 2023
-                        }
-                        .onChange(of: customDate) { newDate in
-                            newEventDate = newDate
-                        }
-                    }
-                    
-                    
-                    
-                    
-                    Section(header: Text("Event Type").formRegularStyle()) {
-                        Picker("Select Event Type", selection: $newEventType) {
-                            ForEach(newEventTypeOptions, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                    }
-                    
-                    // Save Button --------------------------------------------------------------------
-                    
-                    Section {
-                        VStack {
-                            
-                            Button(action: addItem) {
-                                HStack {
-                                    Spacer()
-                                    Image(systemName: "calendar.badge.plus")
-                                        .font(.system(size: 30))
-                                        .padding(.trailing, 10)
-                                    Text("Save")
-                                        .formSaveButtonStyle()
-                                    Spacer()
-                                }
-                                .padding(10)
+                Section(header: Text("Event Date, upcoming or past").formRegularStyle()) {
+                    VStack {
+                        Picker("Month", selection: $selectedMonth) {
+                            ForEach(months, id: \.self) { month in
+                                Text(month).tag(month)
                                 
-                                .frame(minWidth: 0, maxWidth: .infinity)
                             }
-                            .background(Color.formHeaderAndSaveButtonBackground)
-                            .foregroundColor(.formHeaderAndSaveButtonForeground)
-                            .cornerRadius(10)
-                            
+                        }
+                        
+                        
+                        Picker("Day", selection: $selectedDay) {
+                            ForEach(1..<32) { day in
+                                Text("\(day)").tag(day)
+                                
+                            }
+                        }
+                        
+                        
+                        Picker("Year", selection: $selectedYear) {
+                            ForEach(1920..<2100) { year in
+                                Text(String(format: "%ld", locale: Locale(identifier: "en_US_POSIX"), year)).tag(year)
+                                
+                                
+                            }
+                        }
+                        
+                        
+                    }
+                    .onAppear {
+                        // Initialize the custom picker with the existing date
+                        let components = Calendar.current.dateComponents([.day, .month, .year], from: newEventDate)
+                        selectedMonth = months[(components.month ?? 1) - 1]
+                        selectedDay = components.day ?? 1
+                        selectedYear = components.year ?? 2023
+                    }
+                    .onChange(of: customDate) { newDate in
+                        newEventDate = newDate
+                    }
+                }
+                
+                
+                
+                
+                Section(header: Text("Event Type").formRegularStyle()) {
+                    Picker("Select Event Type", selection: $newEventType) {
+                        ForEach(newEventTypeOptions, id: \.self) {
+                            Text($0)
                         }
                     }
-                    .frame(minHeight: 0, maxHeight: .infinity)
-                    
-                    // Save Button End ------------------------------------------------------------------
-                    
                 }
                 
+                // Save Button --------------------------------------------------------------------
                 
-                
-                
-                
-                // Alert ---------------------------------------------------------------------------
-                
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Error"),
-                          message: Text("Name field is required."),
-                          dismissButton: .default(Text("OK")))
+                Section {
+                    VStack {
+                        
+                        Button(action: addItem) {
+                            HStack {
+                                Spacer()
+                                Image(systemName: "calendar.badge.plus")
+                                    .font(.system(size: 30))
+                                    .padding(.trailing, 10)
+                                Text("Save")
+                                    .formSaveButtonStyle()
+                                Spacer()
+                            }
+                            .padding(10)
+                            
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                        }
+                        .background(Color.formHeaderAndSaveButtonBackground)
+                        .foregroundColor(.formHeaderAndSaveButtonForeground)
+                        .cornerRadius(10)
+                        
+                    }
+                    
                 }
+                .frame(minHeight: 0, maxHeight: .infinity)
                 
-                // Alert End -----------------------------------------------------------------------
+                // Save Button End ------------------------------------------------------------------
+                
             }
-            .background(Color.formHeaderAndSaveButtonBackground)
+            
+            
+            
+            
+            
+            
+            // Alert ---------------------------------------------------------------------------
+            
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Error"),
+                      message: Text("Name field is required."),
+                      dismissButton: .default(Text("OK")))
+            }
+            
+            // Alert End -----------------------------------------------------------------------
         }
+        .background(Color.formHeaderAndSaveButtonBackground)
         
     }
+    
+    
+    
+    
     
     
     private func addItem() {
@@ -188,7 +194,7 @@ struct NewDataEntryForm: View {
             showAlert = true
             return
         }
-
+        
         let newItem = Item(context: viewContext)
         let uuid = UUID().uuidString
         
@@ -207,7 +213,7 @@ struct NewDataEntryForm: View {
             // Handle the error appropriately
         }
     }
-
+    
 }
 
 
