78d2a42c1a2a16d9c3099fba59dc3980ea6023f2
Cody <66153585+nonstickcode@users.noreply.github.com>
2023-09-11 12:38:38 -0700

    combine the function to switch color for background and text for calander icon

--- Date Tracker/App Styles/AppColors.swift
+++ Date Tracker/App Styles/AppColors.swift
@@ -14,13 +14,9 @@ extension Color {
     static let mainFooterTextColor = Color.white
     static let formHeaderAndSaveButtonForeground = Color.black
     static let formHeaderAndSaveButtonBackground = Color.green.opacity(0.8)
-}
 
 
 
-
-
-extension Color {
     init(hex: String) {
         let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int: UInt64 = 0
@@ -31,8 +27,6 @@ extension Color {
             (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
         case 6: // RGB (24-bit)
             (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
         default:
             (a, r, g, b) = (255, 0, 0, 0)
         }
--- Date Tracker/Components/CalanderIconView.swift
+++ Date Tracker/Components/CalanderIconView.swift
@@ -4,44 +4,43 @@ struct CalendarIconView: View {
     var month: String
     var day: String
     
-    func getColor(for month: String) -> Color {
+    func getColor(for month: String) -> (bgColor: Color, textColor: Color) {
         switch month.lowercased() {
         case "jan":
-            return Color(hex: "FFB347") // Caramel
+            return (Color(hex: "FFB347"), Color(hex: "000000")) // Caramel
         case "feb":
-            return Color(hex: "DEA5A4") // Sheer Lilac
+            return (Color(hex: "DEA5A4"), Color(hex: "000000")) // Sheer Lilac
         case "mar":
-            return Color(hex: "77DD77") // Fair Aqua
+            return (Color(hex: "77DD77"), Color(hex: "000000")) // Fair Aqua
         case "apr":
-            return Color(hex: "E97451") // Cayenne
+            return (Color(hex: "E97451"), Color(hex: "FFFFFF")) // Cayenne
         case "may":
-            return Color(hex: "C23B22") // Bud Green
+            return (Color(hex: "C23B22"), Color(hex: "FFFFFF")) // Bud Green
         case "jun":
-            return Color(hex: "FFD700") // Aspen Gold
+            return (Color(hex: "FFD700"), Color(hex: "000000")) // Aspen Gold
         case "jul":
-            return Color(hex: "FF6961") // Coral Blush
+            return (Color(hex: "FF6961"), Color(hex: "FFFFFF")) // Coral Blush
         case "aug":
-            return Color(hex: "FF7518") // Sun Orange
+            return (Color(hex: "FF7518"), Color(hex: "FFFFFF")) // Sun Orange
         case "sep":
-            return Color(hex: "1DACD6") // Baja Blue
+            return (Color(hex: "1DACD6"), Color(hex: "FFFFFF")) // Baja Blue
         case "oct":
-            return Color(hex: "9FE2BF") // Cerulean
+            return (Color(hex: "000000"), Color(hex: "FFFF00")) // Halloween Black and Yellow
         case "nov":
-            return Color(hex: "C21E56") // Claret Red
+            return (Color(hex: "C21E56"), Color(hex: "FFFFFF")) // Claret Red
         case "dec":
-            return Color(hex: "01796F") // Pagoda Blue
+            return (Color(hex: "01796F"), Color(hex: "FFFFFF")) // Pagoda Blue
         default:
-            return Color.gray
+            return (Color(hex: "808080"), Color(hex: "000000"))
         }
     }
 
-    func getTextColor(for backgroundColor: Color) -> Color {
-        return Color.white // Adjust this as you like
-    }
+   
     
     var body: some View {
-        let backgroundColor = getColor(for: month)
-        let textColor = getTextColor(for: backgroundColor)
+        let colors = getColor(for: month)
+        let backgroundColor = colors.bgColor
+        let textColor = colors.textColor
         
         return ZStack(alignment: .top) {
             Rectangle()
